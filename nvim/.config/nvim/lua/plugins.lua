5ed5015ba02ec26faabd2fbc40f2b22e0c6fe84a
Nathan Knotts <nknotts@gmail.com>
2024-08-07 07:47:23 -0400

    disable auto theme change on ssh

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -7,7 +7,11 @@ return {
     end,
   },  
   { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" },
-  "vimpostor/vim-lumen",
+  { "vimpostor/vim-lumen",
+    enabled = function()
+      return os.getenv("SSH_TTY") == nil
+    end
+  },
   "neovim/nvim-lspconfig",
   "simrat39/rust-tools.nvim"
 }
