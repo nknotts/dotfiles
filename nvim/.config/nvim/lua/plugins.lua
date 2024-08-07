9fbf9162106f4f5a056b1c26a12e8bb59c4bf93d
Nathan Knotts <nknotts@gmail.com>
2024-08-07 05:30:05 -0400

    nvim: update theming

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,16 +1,20 @@
 return {
-  { "catppuccin/nvim", as = "catppuccin" },
+  { "catppuccin/nvim",
+    name = "catppuccin", 
+    priority = 1000,
+    config = function()
+      vim.cmd("colorscheme catppuccin")
+    end,
+  },  
   { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" },
   { "f-person/auto-dark-mode.nvim",
     opts = {
-      update_interval = 1000,
+      update_interval = 2000,
       set_dark_mode = function()
-        vim.api.nvim_set_option_value("background", "dark", {})
-        vim.cmd("colorscheme catppuccin")
+        vim.api.nvim_set_option("background", "dark")
       end,
       set_light_mode = function()
-        vim.api.nvim_set_option_value("background", "light", {})
-        vim.cmd("colorscheme catppuccin")
+        vim.api.nvim_set_option("background", "light")
       end,
     },
   },
