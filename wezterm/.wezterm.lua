ab0195a778bea675a6f1abc31bd58425fe2253e5
Nathan Knotts <nknotts@gmail.com>
2024-08-09 07:56:51 -0400

    wezterm: do not close when all windows are closed
    reformat text

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,12 +2,12 @@
 local wezterm = require 'wezterm'
 
 
-function scheme_for_appearance(appearance)
-  if appearance:find "Dark" then
-    return "Catppuccin Mocha"
-  else
-    return "Catppuccin Latte"
-  end
+local function scheme_for_appearance(appearance)
+    if appearance:find "Dark" then
+        return "Catppuccin Mocha"
+    else
+        return "Catppuccin Latte"
+    end
 end
 
 -- This will hold the configuration.
@@ -15,18 +15,18 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
--- For example, changing the color scheme:
-config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+config.quit_when_all_windows_are_closed = false
 
+config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
 
 config.window_background_opacity = 0.95
 
 config.keys = {
-  {
-    key = 'Space',
-    mods = 'CTRL',
-    action = wezterm.action.SendKey { key = 'b', mods = 'CTRL' }
-  }
+    {
+        key = 'Space',
+        mods = 'CTRL',
+        action = wezterm.action.SendKey { key = 'b', mods = 'CTRL' }
+    }
 }
 
 -- and finally, return the configuration to wezterm
